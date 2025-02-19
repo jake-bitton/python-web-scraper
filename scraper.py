a0d520f761cf5198053d53ceecb941ef8a6432a6
Jake <jbitton@unc.edu>
2025-02-19 12:14:30 -0500

    addedd functionality allowing a list of urls to be passed to Scrape() and updates file write formatting

--- scraper.py
+++ scraper.py
@@ -4,7 +4,10 @@ import io
 
 
 
-def Scrape(url: str, tag: str='a', get_val: str='href'):
+def Scrape(urls: str | list[str], tag: str='a', get_val: str='href'):
+    
+    assert urls.isinstance(list[str] | str)
+
     f = None
     try:
         f = open("site_data.txt", "a")
@@ -13,19 +16,23 @@ def Scrape(url: str, tag: str='a', get_val: str='href'):
     finally:
         if f is not None:
             f.close()
+
+    #   Converts str url into a 1 elem list if necessary and then scrapes each item in the list of urls.
+    if urls.isinstance(str):
+        url = [urls]
             
-    #  Target URL
+    for l in url | urls:
+        response = requests.get(l)
+        f.write(f'Now writing from {l}:')
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
 
-    response = requests.get(url)
+            # Extract all of desired HTML tag
+            tags = soup.find_all(tag)
+            for t in tags:
+                f.write(f'\t{t}\n')
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        # Extract all of desired HTML tag
-        tags = soup.find_all(tag)
-        for t in tags:
-            f.write(f'{t}\n')
-        f.close()
-    else:
-        print(f'Error: {response.status_code}')
-        f.close()
+        else:
+            f.write(f'\tError: {response.status_code} in URL: {l}\n')
+    
+    f.close()
